38c70da67170ad21591f65467f4f28ed61d914f0
Pintuprusty16@gmail.com <pintuprusty16@gmail.com>
2023-11-02 15:01:10 +0530

    chai_aur_react#5(counter)

--- counter01/src/App.jsx
+++ counter01/src/App.jsx
@@ -1,39 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [count_, getcount] = useState(0)
+  let [counter, setColor] = useState(0);
+  const addValue = () => {
+    if(counter<=20){
 
+     setColor(counter++);
+  
+      console.log();
+    }
+  };
+    const remove=()=>{
+      if(counter>=0){
+        setColor(counter--)
+      }
+    }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          add {count}
-        </button>
-        <button onClick={() => getcount((count_) => count_ - 1)}>
-          remove {count_}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>My favorite Number is {counter} !</h1>
+      <button onClick={addValue}>add</button>
+      <button onClick={remove}>remove</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
